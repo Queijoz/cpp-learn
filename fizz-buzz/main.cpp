9c8b1afc60b5732eb9e2c3cd0abb3aaf278da79f
Luan Queiroz <lqggamer@outlook.com>
2019-07-25 16:17:08 -0300

    Improving Fizz Buzz

--- fizz-buzz/main.cpp
+++ fizz-buzz/main.cpp
@@ -1,26 +1,18 @@
 #include <stdio.h> // Standard input output header;
+#include <string.h> // Using for string manipulate;
 #include <unistd.h> // Using for sleep function;
 int main(){
-  printf("Starting Fizz Buzz\n");
+  printf("Starting Fizz Buzz\n\n");
   sleep(2);
 
   for (int counter = 1; counter > 0; counter++) {
-    // Loop for keep printing numbers. The counter is the number which is gonna be printed.
-    if (counter % 3 == 0 && counter % 5 == 0) {
-      // If the counter is divisible by 3 and 5 without remainder numbers, print FizzBuzz.
-      printf("FiZzBuZz\n");
-    }else{
-      // If the counter is multiple of 3, print Buzz. If multiple of 5, print Fizz.
-      if (counter % 3 == 0) {
-        printf("Buzz\n");
-      }else{
-        if (counter % 5 == 0) {
-          printf("Fizz\n");
-        }else{
-          printf("%i\n", counter);
-        }
-      }
-    }
+    // Loop for keep printing numbers.
+    char output[8] = "";
+    if (counter % 3 == 0) { strcat(output, "Fizz"); }
+    if (counter % 5 == 0) { strcat(output, "Buzz"); }
+    if (strcmp(output, "") == 0) { printf("%d\n", counter); sleep(1); continue; }
+
+    printf("%s\n", output);
     sleep(1);
   }
 }
